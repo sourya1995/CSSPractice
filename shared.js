9d6844f0c6405db9f3309f03b54b67d95c42179a
Sourya Bhattacharya <souryabhattacharya21@gmail.com>
2023-10-30 15:31:09 +0530

    slider menu finished

--- shared.js
+++ shared.js
@@ -2,20 +2,43 @@ var backdrop = document.querySelector(".backdrop");
 var modal = document.querySelector(".modal");
 var modalNoButton = document.querySelector(".modal__action--negative");
 var selectPlanButtons = document.querySelectorAll(".plan button");
+var toggleButton = document.querySelector(".toggle-button");
+var mobileNav = document.querySelector(".mobile-nav");
 
 // backdrop.style.display = 'block';
 
 for (var i = 0; i < selectPlanButtons.length; i++) {
   selectPlanButtons[i].addEventListener("click", function () {
-    modal.style.display = "block";
-    backdrop.style.display = "block";
+    // modal.style.display = "block";
+    // backdrop.style.display = "block";
+    // modal.className = 'open';
+    modal.classList.add("open");
+    backdrop.classList.add("open");
   });
 }
 
-backdrop.addEventListener("click", closeModal);
+backdrop.addEventListener("click", function () {
+//   mobileNav.style.display = "none";
+  mobileNav.classList.remove("open");
+  this.closeModal();
+});
 modalNoButton.addEventListener("click", closeModal);
 
-function closeModal() {
-  backdrop.style.display = "none";
-  modalNo.style.display = "none";
+if(modalNoButton){
+    modalNoButton.addEventListener("click", closeModal);
 }
+
+function closeModal() {
+  //   backdrop.style.display = "none";
+  //   modalNoButton.style.display = "none";
+  if(modal){
+     modal.classList.remove("open");
+  }
+ 
+  backdrop.classList.remove("open");
+}
+
+toggleButton.addEventListener("click", function () {
+  mobileNav.classList.add('open');
+  backdrop.classList.add('open');
+});
